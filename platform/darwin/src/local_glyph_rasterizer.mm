2b8bb4b78d1b6bbd3dd7e9c73543a176ba5c9d4f
Minh Nguyễn <mxn@1ec5.org>
2020-04-16 11:06:26 -0700

    [core, ios, macos] Shape text using Core Text
    Started implementing a Core Text–based iOS/macOS shaper separate from the default implementation in C++.
    
    Consult the local glyph rasterizer to translate Unicode codepoints to glyph indices.
    
    Temporarily reverted many of the metrics improvements in the glyph rasterizer to narrow down coordinate transformation issues.

--- platform/darwin/src/local_glyph_rasterizer.mm
+++ platform/darwin/src/local_glyph_rasterizer.mm
@@ -4,6 +4,7 @@
 #include <mbgl/util/constants.hpp>
 
 #include <unordered_map>
+#include <iterator>
 
 #import <Foundation/Foundation.h>
 #import <CoreText/CoreText.h>
@@ -52,6 +53,8 @@ using CTFontRefHandle = CFHandle<CTFontRef, CFTypeRef, CFRelease>;
 using CTFontDescriptorRefHandle = CFHandle<CTFontDescriptorRef, CFTypeRef, CFRelease>;
 using CTLineRefHandle = CFHandle<CTLineRef, CFTypeRef, CFRelease>;
 
+CTFontDescriptorRef createFontDescriptor(const FontStack& fontStack, NSArray<NSString *>* fallbackFontNames, bool isVertical);
+
 class LocalGlyphRasterizer::Impl {
 public:
     Impl(const optional<std::string> fontFamily_)
@@ -66,49 +69,6 @@ public:
     }
     
     bool isEnabled() { return fallbackFontNames; }
-    
-    CTFontDescriptorRef createFontDescriptor(const FontStack& fontStack) {
-        NSMutableArray *fontNames = [NSMutableArray arrayWithCapacity:fontStack.size() + fallbackFontNames.count];
-        for (auto& fontName : fontStack) {
-            if (fontName != util::LAST_RESORT_ALPHABETIC_FONT && fontName != util::LAST_RESORT_PAN_UNICODE_FONT) {
-                [fontNames addObject:@(fontName.c_str())];
-            }
-        }
-        [fontNames addObjectsFromArray:fallbackFontNames];
-        
-        CFMutableArrayRefHandle fontDescriptors(CFArrayCreateMutable(kCFAllocatorDefault, fontNames.count, &kCFTypeArrayCallBacks));
-        for (NSString *name in fontNames) {
-            NSDictionary *fontAttributes = @{
-                (NSString *)kCTFontSizeAttribute: @(util::ONE_EM),
-                (NSString *)kCTFontNameAttribute: name,
-                (NSString *)kCTFontDisplayNameAttribute: name,
-                (NSString *)kCTFontFamilyNameAttribute: name,
-            };
-            
-            CTFontDescriptorRefHandle descriptor(CTFontDescriptorCreateWithAttributes((CFDictionaryRef)fontAttributes));
-            CFArrayAppendValue(*fontDescriptors, *descriptor);
-        }
-
-        CFStringRef keys[] = { kCTFontSizeAttribute,                  kCTFontCascadeListAttribute };
-        CFTypeRef values[] = { (__bridge CFNumberRef)@(util::ONE_EM), *fontDescriptors };
-
-        CFDictionaryRefHandle attributes(
-            CFDictionaryCreate(kCFAllocatorDefault, (const void**)&keys,
-                (const void**)&values, sizeof(keys) / sizeof(keys[0]),
-                &kCFTypeDictionaryKeyCallBacks,
-                &kCFTypeDictionaryValueCallBacks));
-        if (CFArrayGetCount(*fontDescriptors)) {
-            CTFontDescriptorRef firstDescriptor = (CTFontDescriptorRef)CFArrayGetValueAtIndex(*fontDescriptors, 0);
-            return CTFontDescriptorCreateCopyWithAttributes(firstDescriptor, *attributes);
-        } else {
-            return CTFontDescriptorCreateWithAttributes(*attributes);
-        }
-    }
-
-    CTFontRef createFont(const FontStack& fontStack) {
-        CTFontDescriptorRefHandle descriptor(createFontDescriptor(fontStack));
-        return CTFontCreateWithFontDescriptor(*descriptor, 0.0, NULL);
-    }
     
 private:
     NSArray<NSString *> *fallbackFontNames;
@@ -125,25 +85,8 @@ bool LocalGlyphRasterizer::canRasterizeGlyph(const FontStack&, GlyphID) {
     return impl->isEnabled();
 }
 
-PremultipliedImage drawGlyphBitmap(GlyphID glyphID, CTFontRef font, GlyphMetrics& metrics) {
-    CFStringRefHandle string(CFStringCreateWithCharacters(NULL, reinterpret_cast<UniChar*>(&glyphID), 1));
-
-    CFStringRef keys[] = { kCTFontAttributeName };
-    CFTypeRef values[] = { font };
-
-    CFDictionaryRefHandle attributes(
-        CFDictionaryCreate(kCFAllocatorDefault, (const void**)&keys,
-            (const void**)&values, sizeof(keys) / sizeof(keys[0]),
-            &kCFTypeDictionaryKeyCallBacks,
-            &kCFTypeDictionaryValueCallBacks));
-
-    CFAttributedStringRefHandle attrString(CFAttributedStringCreate(kCFAllocatorDefault, *string, *attributes));
-    CTLineRefHandle line(CTLineCreateWithAttributedString(*attrString));
-    
-    Size size(35, 35);
-    metrics.width = size.width;
-    metrics.height = size.height;
-    
+PremultipliedImage drawGlyphBitmap(GlyphID glyphID, CTFontRef font, Size size) {
+    CGGlyph glyphs[] = { glyphID };
     PremultipliedImage rgbaBitmap(size);
     
     CGColorSpaceHandle colorSpace(CGColorSpaceCreateDeviceRGB());
@@ -167,45 +110,41 @@ PremultipliedImage drawGlyphBitmap(GlyphID glyphID, CTFontRef font, GlyphMetrics
         throw std::runtime_error("CGBitmapContextCreate failed");
     }
     
-    CFArrayRef glyphRuns = CTLineGetGlyphRuns(*line);
-    CTRunRef glyphRun = (CTRunRef)CFArrayGetValueAtIndex(glyphRuns, 0);
-    CFRange wholeRunRange = CFRangeMake(0, CTRunGetGlyphCount(glyphRun));
-    CGSize advances[wholeRunRange.length];
-    CTRunGetAdvances(glyphRun, wholeRunRange, advances);
-    metrics.advance = advances[0].width;
-    
-    CGFloat ascent;
-    CGFloat descent;
-    CGFloat leading;
-    CTRunGetTypographicBounds(glyphRun, wholeRunRange, &ascent, &descent, &leading);
-    
-    // Start drawing a little bit below the top of the bitmap
-    CGContextSetTextPosition(*context, 0.0, descent);
-//    CTLineDraw(*line, *context);
-    
-    CTFontRef glyphFont = (CTFontRef)CFDictionaryGetValue(CTRunGetAttributes(glyphRun), kCTFontAttributeName);
-    
-    CGGlyph glyphs[wholeRunRange.length];
-    CTRunGetGlyphs(glyphRun, wholeRunRange, glyphs);
-    CGPoint positions[wholeRunRange.length];
-    CTRunGetPositions(glyphRun, wholeRunRange, positions);
-    CTFontDrawGlyphs(glyphFont, glyphs, positions, wholeRunRange.length, *context);
+    CGPoint positions[1];
+    positions[0] = CGPointMake(0.0, 5.0);
+    CTFontDrawGlyphs(font, glyphs, positions, 1, *context);
     
     return rgbaBitmap;
 }
 
 Glyph LocalGlyphRasterizer::rasterizeGlyph(const FontStack& fontStack, GlyphID glyphID) {
     Glyph manufacturedGlyph;
-    CTFontRefHandle font(impl->createFont(fontStack));
-    if (!font) {
-        return manufacturedGlyph;
-    }
+    CFStringRef fontName = (__bridge CFStringRef)@(fontStack.front().c_str());
+    CTFontRefHandle font(CTFontCreateWithName(fontName, 0.0, NULL));
     
     manufacturedGlyph.id = glyphID;
 
-    PremultipliedImage rgbaBitmap = drawGlyphBitmap(glyphID, *font, manufacturedGlyph.metrics);
-   
+    // TODO: Plumb through vertical text.
+    CTFontOrientation orientation = kCTFontOrientationHorizontal;
+    
+    CGRect boundingRects[1];
+    CGGlyph glyphs[] = { glyphID };
+    CGRect boundingRect = CTFontGetBoundingRectsForGlyphs(*font, orientation, glyphs, boundingRects, 1);
+    if (CGRectIsNull(boundingRect)) {
+        throw std::runtime_error("CTFontGetBoundingRectsForGlyphs failed");
+    }
+    manufacturedGlyph.metrics.left = 3;
+    manufacturedGlyph.metrics.top = -1;
+    manufacturedGlyph.metrics.width = 35;
+    manufacturedGlyph.metrics.height = 35;
+    
+    CGSize advances[1];
+    CTFontGetAdvancesForGlyphs(*font, orientation, glyphs, advances, 1);
+    manufacturedGlyph.metrics.advance = 24;
+    
     Size size(manufacturedGlyph.metrics.width, manufacturedGlyph.metrics.height);
+    PremultipliedImage rgbaBitmap = drawGlyphBitmap(glyphID, *font, size);
+   
     // Copy alpha values from RGBA bitmap into the AlphaImage output
     manufacturedGlyph.bitmap = AlphaImage(size);
     for (uint32_t i = 0; i < size.width * size.height; i++) {
@@ -213,6 +152,84 @@ Glyph LocalGlyphRasterizer::rasterizeGlyph(const FontStack& fontStack, GlyphID g
     }
 
     return manufacturedGlyph;
+}
+
+CTFontDescriptorRef createFontDescriptor(const FontStack& fontStack, NSArray<NSString *>* fallbackFontNames, bool isVertical) {
+    NSMutableArray *fontNames = [NSMutableArray arrayWithCapacity:fontStack.size() + fallbackFontNames.count];
+    for (auto& fontName : fontStack) {
+        if (fontName != util::LAST_RESORT_ALPHABETIC_FONT && fontName != util::LAST_RESORT_PAN_UNICODE_FONT) {
+            [fontNames addObject:@(fontName.c_str())];
+        }
+    }
+    [fontNames addObjectsFromArray:fallbackFontNames];
+    
+    CFMutableArrayRefHandle fontDescriptors(CFArrayCreateMutable(kCFAllocatorDefault, fontNames.count, &kCFTypeArrayCallBacks));
+    for (NSString *name in fontNames) {
+        NSDictionary *fontAttributes = @{
+            (NSString *)kCTFontSizeAttribute: @(util::ONE_EM),
+            (NSString *)kCTFontNameAttribute: name,
+            (NSString *)kCTFontDisplayNameAttribute: name,
+            (NSString *)kCTFontFamilyNameAttribute: name,
+        };
+        
+        CTFontDescriptorRefHandle descriptor(CTFontDescriptorCreateWithAttributes((CFDictionaryRef)fontAttributes));
+        CFArrayAppendValue(*fontDescriptors, *descriptor);
+    }
+    
+    CTFontOrientation orientation = isVertical ? kCTFontOrientationVertical : kCTFontOrientationHorizontal;
+
+    CFStringRef keys[] = { kCTFontSizeAttribute,                  kCTFontCascadeListAttribute, kCTFontOrientationAttribute };
+    CFTypeRef values[] = { (__bridge CFNumberRef)@(util::ONE_EM), *fontDescriptors,            (__bridge CFNumberRef)@(orientation) };
+
+    CFDictionaryRefHandle attributes(
+        CFDictionaryCreate(kCFAllocatorDefault, (const void**)&keys,
+            (const void**)&values, sizeof(keys) / sizeof(keys[0]),
+            &kCFTypeDictionaryKeyCallBacks,
+            &kCFTypeDictionaryValueCallBacks));
+    if (CFArrayGetCount(*fontDescriptors)) {
+        CTFontDescriptorRef firstDescriptor = (CTFontDescriptorRef)CFArrayGetValueAtIndex(*fontDescriptors, 0);
+        return CTFontDescriptorCreateCopyWithAttributes(firstDescriptor, *attributes);
+    } else {
+        return CTFontDescriptorCreateWithAttributes(*attributes);
+    }
+}
+
+GlyphDependencies getGlyphDependencies(const FontStack& fontStack, const std::string& text, bool isVertical) {
+    // TODO: Implement global fallback fonts.
+    CTFontDescriptorRefHandle descriptor(createFontDescriptor(fontStack, @[], isVertical));
+    CTFontRefHandle font(CTFontCreateWithFontDescriptor(*descriptor, 0.0, NULL));
+
+    CFStringRef keys[] = { kCTFontAttributeName };
+    CFTypeRef values[] = { *font };
+
+    CFDictionaryRefHandle attributes(
+        CFDictionaryCreate(kCFAllocatorDefault, (const void**)&keys,
+            (const void**)&values, sizeof(keys) / sizeof(keys[0]),
+            &kCFTypeDictionaryKeyCallBacks,
+            &kCFTypeDictionaryValueCallBacks));
+
+    CFStringRef string = (__bridge CFStringRef)@(text.c_str());
+    CFAttributedStringRefHandle attrString(CFAttributedStringCreate(kCFAllocatorDefault, string, *attributes));
+    CTLineRefHandle line(CTLineCreateWithAttributedString(*attrString));
+    
+    CFArrayRef glyphRuns = CTLineGetGlyphRuns(*line);
+    GlyphDependencies dependencies;
+    for (CFIndex i = 0; i < CFArrayGetCount(glyphRuns); i++) {
+        CTRunRef glyphRun = (CTRunRef)CFArrayGetValueAtIndex(glyphRuns, 0);
+        CFRange wholeRunRange = CFRangeMake(0, CTRunGetGlyphCount(glyphRun));
+        
+        CTFontRef glyphFont = (CTFontRef)CFDictionaryGetValue(CTRunGetAttributes(glyphRun), kCTFontAttributeName);
+        CFStringRefHandle glyphFontName(CTFontCopyName(glyphFont, kCTFontPostScriptNameKey));
+        FontStack glyphFontStack = {{ [(__bridge NSString *)*glyphFontName UTF8String] }};
+        
+        // Use CTRunGetGlyphsPtr() if available.
+        CGGlyph glyphs[wholeRunRange.length];
+        CTRunGetGlyphs(glyphRun, wholeRunRange, glyphs);
+        
+        GlyphIDs& glyphIDs = dependencies[glyphFontStack];
+        glyphIDs.insert(glyphs, glyphs + wholeRunRange.length);
+    }
+    return dependencies;
 }
 
 } // namespace mbgl
